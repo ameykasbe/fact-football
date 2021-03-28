70d56588d3cbd5cc692aaa4e3da568f859961c05
Amey Kasbe <amey.kasbe@gmail.com>
2021-03-28 10:54:03 +0530

    Add API Overview

--- api/views.py
+++ api/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 
+@api_view(['GET'])
 def api_overview(request):
-    return JsonResponse("API Base Point", safe=False)
+    api_urls = {
+        'List': '/fact-list/',
+        'Detail': '/fact-detail/<str:pk>',
+        'Create': '/fact-create/',
+        'Update': '/fact-update/<str:pk>',
+        'Delete': '/fact-delete/<str:pk>',
+    }
+    return Response(api_urls)
